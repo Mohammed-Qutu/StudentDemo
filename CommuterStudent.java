563e6c489a7f424b155ac61457981baafbd9497f
Mohammed Qutu <q2mohammed@gmail.com>
2024-09-30 14:02:58 -0500

    Create CommuterStudent.java

---
+++ CommuterStudent.java
@@ -0,0 +1,11 @@
+public class CommuterStudent extends Student {
+    private double parkingCost = 0.0;
+
+    public void setParkingCost(double cost) {
+        parkingCost = cost;
+    }
+
+    public double getParkingCost() {
+        return parkingCost;
+    }
+}
