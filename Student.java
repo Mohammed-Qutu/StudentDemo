41c545c2529a3b0bd5018c33d6436669423c573a
Mohammed Qutu <q2mohammed@gmail.com>
2024-09-30 14:02:34 -0500

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,20 @@
+public class Student {
+    private String name;
+    private String major;
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setMajor(String major) {
+        this.major = major;
+    }
+    
+    public String getMajor() {
+        return major;
+    }
+}
