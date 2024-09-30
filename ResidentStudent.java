df99f6b68294ee09fef9bff1da0cd1f3400c4162
Mohammed Qutu <q2mohammed@gmail.com>
2024-09-30 14:03:13 -0500

    Create ResidentStudent.java

---
+++ ResidentStudent.java
@@ -0,0 +1,13 @@
+public class ResidentStudent extends Student {
+    private double dormCost = 0.0;
+
+    // setDormCost method
+    public void setDormCost(double dorm) {
+        dormCost = dorm;
+    }
+
+    // getDormCost method
+    public double getDormCost() {
+        return dormCost;
+    }
+}
